0152b8c68f6d7756083e368525add8eda107af9a
totordudu <toto.duvinage@gmail.com>
2019-12-07 19:11:55 +0100

    add waypoints PointArray

--- src/send_data.cpp
+++ src/send_data.cpp
@@ -1,34 +1,45 @@
 // ROS
 #include <ros/ros.h>
+#include <geometry_msgs/Point.h>
 #include "topological_navigation/TopologicalPath.h"
 #include "topological_navigation/PointArray.h"
-#include <geometry_msgs/Point.h>
-
 
 int main(int argc, char** argv) {
   ros::init(argc, argv,"send_data");
   ros::NodeHandle nh;
-  ros::NodeHandle private_nh("~");
 
-  ros::serviceClient client = private_nh.serviceClient<topological_navigation::TopologicalPath>("topological_path");
-  topological_navigation::TopologicalPath srv;
 
-  geometry_msgs::Points starting, ending;
+  topological_navigation::TopologicalPath::Request req;
+  topological_navigation::TopologicalPath::Response res;
 
-  starting.x = 10;  starting.y = 10;    starting.z = 10;
-  ending.x = 30;    ending.y = 30;      ending.z = 30;
+  topological_navigation::PointArray waypoints;
 
-  srv.request.starting = starting;
-  srv.request.ending = ending;
-  //we also have to send a points list 
+  ros::ServiceClient client = nh.serviceClient<topological_navigation::TopologicalPath>("topological_path");
+  
+  geometry_msgs::Point starting, ending;
+  geometry_msgs::Point waypoints0,waypoints1;
 
-  if(client.call(srv)){
-      ROS_INFO("Topological_navigation get points");
+  starting.x = 10;  starting.y = 10;    starting.z = 0;
+  ending.x = 50;    ending.y = 50;      ending.z = 0;
+
+  waypoints0.x = 20;  waypoints0.y = 10;    waypoints0.z = 0;
+  waypoints1.x = 15;  waypoints1.y = 15;    waypoints1.z = 0;
+
+  waypoints.points.push_back(waypoints0);
+  waypoints.points.push_back(waypoints1);
+
+  req.starting = starting;
+  req.ending = ending;
+  req.waypoints = waypoints;
+
+  if(client.call(req,res)){
+    ROS_INFO("Topological_navigation get all points");
   }
   else
   {
-      ROS_ERROR("Fail to call service get_points");
-      return 1;
+    ROS_ERROR("Fail to call service topological_path");
+    ROS_ERROR("No path found");
+    return false;
   }
 
   return 0;
